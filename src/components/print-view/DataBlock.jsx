133b7e086dcba95823e4c0de3f102788cd9dadc6
createlsworklappy <justinecastillo7@gmail.com>
2025-10-24 16:06:50 +0900

    refactor(print-preview): switch skills grid to flexbox
    refactor(DataBlock): group left and right sides of the data blocks for cleaner layout

--- src/components/print-view/DataBlock.jsx
+++ src/components/print-view/DataBlock.jsx
@@ -2,17 +2,21 @@ function DataBlock({ title, subtitle, dates, location, descriptionList}) {
 
   return (
     <div className="data-block">
-          {title && <p className="data-title">{title}</p>}
-          {subtitle && <p className="data-subtitle">{subtitle}</p>}
-          {dates && <p className="data-dates">{dates}</p>}
-          {location && <p className="data-location">{location}</p>}
-          {descriptionList && descriptionList.length > 0 && (
-            <ul>
-            {descriptionList.map((entry, index)=> (
-              <li key={index}>{entry}</li>
-            ))}
-          </ul>
-          )}
+          <div className="block-left-info">
+            {title && <p className="data-title">{title}</p>}
+            {dates && <p className="data-dates">{dates}</p>}
+            {location && <p className="data-location">{location}</p>}
+          </div>
+          <div className="block-right-info">
+            {subtitle && <p className="data-subtitle">{subtitle}</p>}
+            {descriptionList && descriptionList.length > 0 && (
+              <ul>
+              {descriptionList.map((entry, index)=> (
+                <li key={index}>{entry}</li>
+              ))}
+            </ul>
+            )}
+          </div>
     </div>
   );
 }
