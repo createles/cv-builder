fc0ace6b51e7d1febaa162e93d5a85f4d77ca82a
createlsworklappy <justinecastillo7@gmail.com>
2025-10-24 16:54:35 +0900

    refactor: set gaps within data blocks
    feat: allow enter key to move within inputs in a form

--- src/components/input-forms/InputItem.jsx
+++ src/components/input-forms/InputItem.jsx
@@ -1,12 +1,38 @@
 import '../../styles/input-item.css'
 
 function InputItem({label, type, name, value, onChange, required}) {
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter" && type !== 'textarea' ) {
+            event.preventDefault();
+
+            const form = event.target.form;
+            if (!form) return;
+
+            const focusables = Array.from(
+            form.querySelectorAll('input, textarea, select, button:not([disabled])')
+            );
+
+            const currentIndex = focusables.indexOf(event.target);
+
+            const nextIndex = currentIndex + 1;
+
+            if (nextIndex < focusables.length) {
+            focusables[nextIndex].focus();
+            } else {
+                const saveButton = form.querySelector('.save-btn')
+                if (saveButton) {
+                    saveButton.focus();
+                }
+            }
+        };
+    }
+
     return (
         <div className="input-item">
             <label className="item-label" htmlFor={name+"-input"} > {label} </label>
             {type === "textarea" ?
-            <textarea id={name+"-input"} name={name} value={value} onChange={onChange} required={required}></textarea> :
-            <input type={type} name={name} id={name+"-input"} value={value} onChange={onChange} required={required} />}
+            <textarea id={name+"-input"} name={name} value={value} onChange={onChange} required={required} onKeyDown={handleKeyDown}></textarea> :
+            <input type={type} name={name} id={name+"-input"} value={value} onChange={onChange} required={required} onKeyDown={handleKeyDown}/>}
         </div>
     )
 }
